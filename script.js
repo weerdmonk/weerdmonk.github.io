3a426c1bf048e53a9bde35a7cf05ccc80e514f3b
Jyotiprakash Sahoo <jp.sahoo1991@gmail.com>
2017-05-18 11:30:18 +0530

    Added highlights for menu items.

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 
+var texts_to_hide, menu_items;
+
 window.onload = function() {
-  show_menu_text(null, 'home_text');
+  texts_to_hide = document.getElementsByClassName('menu_text');
+  menu_items = document.getElementsByClassName('menu_item');
+
+  show_menu_text(menu_items[0], 'home_text', false);
 }
 
 function hide_menu_text()
 {
-  var texts_to_hide = document.getElementsByClassName('menu_text');
   var idx;
 
   for (i = 0; i < texts_to_hide.length; i++)
   {
     texts_to_hide[i].style.display = 'none';
+    menu_items[i].classList.remove('menu_item_active');
   }
 }
 
-function show_menu_text(menu_item, text_item_show)
+function show_menu_text(menu_item, text_item_show, hide_menu_texts = true)
 {
-  if (menu_item != null)
+  if (hide_menu_texts == true)
   {
     hide_menu_text();
+  }
+
+  if (menu_item != null)
+  {
+    menu_item.classList.add('menu_item_active');
   }
 
   var text_to_show = document.getElementById(text_item_show);
