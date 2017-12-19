01829d11836633a82e11a8929ccae1ca20337135
Jyotiprakash Sahoo <jp.sahoo1991@gmail.com>
2017-12-19 19:31:25 +0530

    Use Github API for list of repos

--- script.js
+++ script.js
@@ -35,3 +35,57 @@ function show_menu_text(menu_item, text_item_show, hide_menu_texts = true)
   text_to_show.style.display = 'block';
 }
 
+/*
+function httpCallback(text)
+{
+  var obj = JSON.parse(text);
+  var paragraph = document.createElement('p');
+  var t = document.createTextNode('some text');
+  paragraph.appendChild(t);
+  document.body.appendChild(paragraph);
+}
+
+function httpGetAsync()
+{
+    var theUrl = 'https://api.github.com/users/weerdmonk/repos'
+    var xmlHttp = new XMLHttpRequest();
+    var text;
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            httpCallback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+*/
+
+function foo(response) {
+  var meta = response.meta;
+  var data = response.data;
+  var number_of_repos = response.data.length;
+  console.log(data);
+	console.log('Number of repos: ' + number_of_repos);
+  console.log('Repos are: ');
+  console.log();
+
+  var repo_list = document.createElement('ul');
+  for (i = 0; i < number_of_repos; i++)
+  {
+    console.log(response.data[i]);
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+    var text = document.createTextNode(response.data[i].name);
+    link.appendChild(text);
+    link.setAttribute('href', response.data[i].html_url);
+    link.setAttribute('target', '_blank');
+    li.appendChild(link);
+    repo_list.appendChild(li);
+  }
+
+  document.getElementById('repos_text').appendChild(repo_list);
+}
+
+var script = document.createElement('script');
+script.src = 'https://api.github.com/users/weerdmonk/repos?callback=foo';
+
+document.getElementsByTagName('head')[0].appendChild(script);
